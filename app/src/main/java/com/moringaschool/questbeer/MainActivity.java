ba5c5f9c1132553b47addcb65577a27126374e82
Esther-Moki <esther.moki@student.moringaschool.com>
2021-10-09 22:35:14 +0300

    added click listener event to the button

--- app/src/main/java/com/moringaschool/questbeer/MainActivity.java
+++ app/src/main/java/com/moringaschool/questbeer/MainActivity.java
@@ -3,12 +3,21 @@ package com.moringaschool.questbeer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mFindBeersButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFindBeersButton= (Button)findViewById(R.id.findBeersButton);
+        mFindBeersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //do something
+            }
+        });
     }
 }
