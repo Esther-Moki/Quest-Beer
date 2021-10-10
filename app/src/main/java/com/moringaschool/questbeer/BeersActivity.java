e91acc9bf2a38d4bce2b81d29358d80ca5049b6e
Esther-Moki <esther.moki@student.moringaschool.com>
2021-10-10 11:15:37 +0300

    added a main activity test and the dependancies

--- app/src/main/java/com/moringaschool/questbeer/BeersActivity.java
+++ app/src/main/java/com/moringaschool/questbeer/BeersActivity.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BeersActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beers);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the pubs near: " + location);
     }
 }
---
+++ app/src/test/java/com/moringaschool/questbeer/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.moringaschool.questbeer;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+    private MainActivity activity;
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+
+    }
+}
