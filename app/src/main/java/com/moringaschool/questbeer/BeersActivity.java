c1bbf8c10c6f393d2a8c35b777ce1ba12d53dc4d
Esther-Moki <esther.moki@student.moringaschool.com>
2021-10-10 16:55:19 +0300

    added a custom array adapter

--- app/src/main/java/com/moringaschool/questbeer/BeersActivity.java
+++ app/src/main/java/com/moringaschool/questbeer/BeersActivity.java
@@ -20,12 +20,17 @@ public class BeersActivity extends AppCompatActivity {
 //    private ListView mListView;
     @BindView(R.id.locationTextView) TextView mLocationTextView;
     @BindView(R.id.listView) ListView mListView;
+    private String[] pubs   = new String[] {"Brew Bistro", " Havana Bar and Restaurant", "Mercury Lounge",
+            "Galileo Lounge", "Klub House (K1)", "Choices Pub and Restaurant", " Bacchus Lounge",
+            "OffRoad Bar and Lounge", " Black Diamond", "Skylux Lounge", "Encore Nairobi", "40Forty Lounge",
+            "G-skyye Lounge", "Milan Kenya club", "Ebony Lounge"};
+
 
     private String[] beers   = new String[] {"Kentucky Brunch Brand Stout", "Pips Meadery",
             "Rare Bourbon County Brand Stout (2010)", "Goose Island Beer Co", "King JJJuliusss", "Blessed",
             "Barrel Aged Adjunct Trail", "Rare Bourbon County Brand Stout (2015)", "The Heart of Darkness",
             "Starry Noche (2020)", "Good Morning", "Assassin (2014) (Gold Wax)",
-            "Double Barrel Hunahpu's", "Bourbon County Brand Vanilla Stout (2010)",};
+            "Double Barrel Hunahpu's", "Bourbon County Brand Vanilla Stout (2010)"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +42,12 @@ public class BeersActivity extends AppCompatActivity {
 //        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
 
         ButterKnife.bind(this);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, beers);
+        //adding custom adapter
+        MyBeersArrayAdapter adapter = new MyBeersArrayAdapter(
+                this, android.R.layout.simple_list_item_1, pubs, beers); // the arguments must match constructor's parameters!
+
+
+        //ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, beers);
         mListView.setAdapter(adapter);
 
       //an OnItemClickListener that will display a toast containing the beer name
--- app/src/main/java/com/moringaschool/questbeer/MyBeersArrayAdapter.java
+++ app/src/main/java/com/moringaschool/questbeer/MyBeersArrayAdapter.java
@@ -1,4 +1,33 @@
 package com.moringaschool.questbeer;
 
-public class MyBeersArrayAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyBeersArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mPubs;
+    private String[] mBeers;
+
+    //adapters take three pieces of info
+
+    public MyBeersArrayAdapter(Context mContext, int resource, String[] mPubs, String[] mBeers) {
+        //Calling super() from a subclass allows you to reference a method that was overridden and also
+        // allows you to call both the original and the overridden method from a subclass, or call
+        // the method from the superclass from within the overriding method from the subclass
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mPubs = mPubs;
+        this.mBeers = mBeers;
+    }
+    @Override
+    public Object getItem(int position) {
+        String pub = mPubs[position];
+        String beer = mBeers[position];
+        return String.format("%s \nServes great: %s", pub, beer);
+    }
+
+    @Override
+    public int getCount() {
+        return mBeers.length;
+    }
 }
