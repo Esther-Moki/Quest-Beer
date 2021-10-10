6fe35b324effd56f4b6f4f27ec5aae0ccad96de5
Esther-Moki <esther.moki@student.moringaschool.com>
2021-10-10 10:38:11 +0300

    added a new intent to direct to the beers class

--- app/src/main/java/com/moringaschool/questbeer/MainActivity.java
+++ app/src/main/java/com/moringaschool/questbeer/MainActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.questbeer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
         mFindBeersButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, BeersActivity.class);
+                startActivity(intent);
+               // Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
             }
         });
     }
