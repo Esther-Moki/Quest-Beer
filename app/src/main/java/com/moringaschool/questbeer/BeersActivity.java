99a854b9484fa4a1a1c5aafa682babff00bf6f81
Esther-Moki <esther.moki@student.moringaschool.com>
2021-10-10 13:33:16 +0300

    added an array adapter to display the beers

--- app/src/main/java/com/moringaschool/questbeer/BeersActivity.java
+++ app/src/main/java/com/moringaschool/questbeer/BeersActivity.java
@@ -7,10 +7,17 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class BeersActivity extends AppCompatActivity {
+
     private TextView mLocationTextView;
+    private String[] beers   = new String[] {"Kentucky Brunch Brand Stout", "Pips Meadery",
+            "Rare Bourbon County Brand Stout (2010)", "Goose Island Beer Co", "King JJJuliusss", "Blessed",
+            "Barrel Aged Adjunct Trail", "Rare Bourbon County Brand Stout (2015)", "The Heart of Darkness",
+            "Starry Noche (2020)", "Good Morning", "Assassin (2014) (Gold Wax)",
+            "Double Barrel Hunahpu's", "Bourbon County Brand Vanilla Stout (2010)",};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beers);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
